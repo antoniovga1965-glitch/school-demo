9aff4c613de3b63277223e26512d3930902fe153
antoniovga1965-glitch <antoniovga1965@gmail.com>
2026-02-20 23:56:07 +0300

    Create winston.js

---
+++ routes/winston.js
@@ -0,0 +1,14 @@
+const {createLogger,format,transports} = require('winston');
+
+const logger =createLogger({
+    level:'info',
+    format:format.combine(
+        format.timestamp('YYYY-MM-DD HH:mm:ss'),
+        format.printf(info=>`${info.message} ${info.timestamp} ${info.level.toUpperCase()}`),
+    ),
+    transports:[
+        new transports.Console(),
+       new transports. File({filename:'app.log',level:'info'})
+    ]
+})
+module.exports=logger;
